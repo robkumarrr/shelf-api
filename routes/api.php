4ea8bc1a3bad1b6bb830f8ddf72bbbe9c1fbe156
robertkumar <rob_kumar@outlook.com>
2026-01-13 09:49:44 -0500

    added v1 api file

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,10 @@ use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Version 1 of API for Resources
+Route::prefix('v1')->group(__DIR__ . '/api_v1.php');
 
+// Authentication
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
-
 Route::post('/logout', LogoutController::class)->middleware('auth:sanctum')->name('logout');
---
+++ routes/api_v1.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
