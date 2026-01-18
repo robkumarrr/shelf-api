a90c1432f27aafc2a3f63cbf9a90249d5eb9a9ee
robertkumar <rob_kumar@outlook.com>
2026-01-17 22:11:59 -0500

    paginate results for index return

--- tests/Feature/Http/Controllers/API/V1/CompactDiscControllerTest.php
+++ tests/Feature/Http/Controllers/API/V1/CompactDiscControllerTest.php
@@ -9,7 +9,23 @@ it('returns all of the compact discs', function() {
     $response = $this->getJson(route('compact-disc.index'));
 
     $response->assertStatus(200)
+        ->assertJsonCount(10, 'data')
         ->assertJsonStructure([
-            'data'
+            'data' => [
+                '*' => ['id', 'album_name', 'artist']
+            ],
+            'links',
+            'meta'
+        ]);
+});
+
+it('returns an empty collection if there are no compact discs', function() {
+    $this->getJson(route('compact-disc.index'))
+        ->assertStatus(200)
+        ->assertJsonCount(0, 'data')
+        ->assertJsonStructure([
+            'data',
+            'links',
+            'meta'
         ]);
 });
