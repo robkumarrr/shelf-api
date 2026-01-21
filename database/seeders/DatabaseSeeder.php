8eba9db8844bc34c0b78d3ae9451d4e568a61fcb
robertkumar <rob_kumar@outlook.com>
2026-01-20 20:59:27 -0500

    improve seeder to include compact discs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CompactDisc;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(50)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        CompactDisc::factory()
+            ->recycle($users)
+            ->count(300)
+            ->create();
     }
 }
