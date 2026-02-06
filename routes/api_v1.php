df360719c9edbbf695488c1085be73f5c247aeff
robertkumar <rob_kumar@outlook.com>
2026-02-05 21:54:42 -0500

    remove unused imports

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\API\V1\CompactDiscController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(["auth:sanctum"])->group(function() {
