cf5e7cafbf29ba5b4ab4124eb794226466a2cebc
robertkumar <rob_kumar@outlook.com>
2026-02-26 22:09:32 -0500

    remove unused imports

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\API\V1\CompactDiscController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/compact-disc/index', [CompactDiscController::class, 'index'])->name('compact-disc.index');
